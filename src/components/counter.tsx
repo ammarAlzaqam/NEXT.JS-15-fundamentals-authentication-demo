422635e056e8aa99f51777978bdc4c67a21bc632
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-14 07:58:51 +0300

    read session and user data

---
+++ src/components/counter.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useAuth, useUser } from "@clerk/nextjs";
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  // const { isLoaded, userId, getToken, sessionClaims } = useAuth();
+  // if (!isLoaded || !userId) return null;
+
+  const { isLoaded, isSignedIn, user } = useUser();
+  if (!isLoaded || !isSignedIn) return null;
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount((prev) => (prev += 1))}>INCREASE</button>
+    </div>
+  );
+}
